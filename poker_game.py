5815806f6df618f317469a93a34872382ec74c23
gpetrosi <gpetrosi@nd.edu>
2019-05-30 18:15:53 +0100

    Poker Game

--- poker_game.py
+++ poker_game.py
@@ -5,7 +5,6 @@ Created on Tue May 28 22:53:20 2019
 
 @author: gracepetrosini
 """
-
 #%%
 import sys
 import random
@@ -27,18 +26,33 @@ class PokerGame:
         print(self.namelist)
 
     def dealer(self):
-        print("calling dealer method")
         self.card_choices = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        self.playerHands = {}
+        self.winnerList = self.namelist.copy()
+        z = 0
         for player in self.namelist:
             self.hand = []
             for value in range(5):
                 self.hand.append(random.choice(self.card_choices))
+                self.hand.sort(reverse = True)
             print(player + ": " + " ".join(str(self.hand)))
-        sort.self.hand
-        #hand__gt__ index[4]
-        
+            self.playerHands[player] = self.hand
+        #print(self.playerHands)
+        while (z<5) and (len(self.winnerList)>1):
+            max = 1
+            for i in self.winnerList:
+                if self.playerHands[i][z] > max:
+                    self.winnerList = [i]
+                    max = self.playerHands[i][z]
+                if self.playerHands[i][z] == max:
+                    if i not in self.winnerList:
+                        self.winnerList.append(i)
+            z += 1
+            
+        print(self.winnerList)
                 
-        
+                    
+
 game1 = PokerGame(playeramount)
 game1.names()
 game1.dealer()
